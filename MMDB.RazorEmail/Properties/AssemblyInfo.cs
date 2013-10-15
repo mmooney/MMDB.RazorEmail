48b8df28d122ea6a8a4c0a84a76d7193c5947c3a
Mike Mooney <mmooney@mmdbsolutions.com>
2013-10-15 08:21:07 -0400

    Upgraded NuGet Packages

--- MMDB.RazorEmail/Properties/AssemblyInfo.cs
+++ MMDB.RazorEmail/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MMDB Solutions, LLC")]
 [assembly: AssemblyProduct("MMDB.RazorEmail")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2013 MMDB Solutions, LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
